7478385e60b78fe6168a4579e4c4c15b05a1f0dd
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-10-06 11:03:43 +0300

    Add external JavaScript file for myjs.html
    Added an external JavaScript file that is linked
    to myjs.html to allow it to be displayed on the
    browser console.

---
+++ javascript-basics/variables-and-operators/myscript.js
@@ -0,0 +1,19 @@
+// let first_name = "John";
+// let surname = "Doe";
+
+// console.log(first_name);
+// console.log(surname);
+
+// let age = 11;
+// console.log(age);
+
+// age = 54;
+// console.log(age);
+
+// const pi = 3.14;
+// pi = 10;
+
+// console.log(pi);
+
+let number = 3 + 2 - 76 * (1 + 1);
+console.log(number);
