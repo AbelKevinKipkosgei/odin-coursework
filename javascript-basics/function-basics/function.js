da03d473cc37cbfd92dc48f4f97c6d548150b0ac
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-10-18 17:47:43 +0300

    Add an example of arrow functions
    Added an example in function.js using arrow
    functions to solidify the concept.

--- javascript-basics/function-basics/function.js
+++ javascript-basics/function-basics/function.js
@@ -5,3 +5,13 @@ function favoriteAnimal(animal) {
 
 const message = favoriteAnimal("Horse");
 console.log(message);
+
+// Arrow function
+let sum = (a, b) => a + b;
+console.log(sum(159, 287));
+
+let age = prompt("What is your age?", 18);
+
+let welcome = age < 18 ? () => alert("Hello!") : () => alert("Greetings!");
+
+welcome();
