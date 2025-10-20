6ab80355144c7f954d66e54c2e4f95440c8a1f23
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-10-20 15:43:31 +0300

    Add a second function to multiply two numbers
    Added another function that takes two numbers,
    multiplies then and displays the result on the
    console.

--- javascript-basics/function-basics/assignment.js
+++ javascript-basics/function-basics/assignment.js
@@ -1,3 +1,9 @@
 // 1
 const add7 = (number) => parseInt(number) + 7;
 console.log(add7(15));
+
+// 2
+const multiply = (number1, number2) => {
+  return number1 * number2;
+};
+console.log(multiply(82, 10));
