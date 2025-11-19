0168bf89b2148bce555c6795112378c39ce8425a
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2025-11-19 03:19:27 +0300

    Filter an array in place
    Filtered an array in place using a for loop and
    splice to modify the original array.

---
+++ javascript-basics/loops-and-arrays/filter-range-in-place.js
@@ -0,0 +1,13 @@
+let arr = [5, 3, 8, 1];
+
+const filterRangeInPlace = (arr, a, b) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < a || arr[i] > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  console.log(arr);
+};
+
+filterRangeInPlace(arr, 1, 4);
