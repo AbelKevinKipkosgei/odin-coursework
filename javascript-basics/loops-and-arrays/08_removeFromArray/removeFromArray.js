07389c6a63c3c4998164502f73a26f9be05d5a64
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2025-11-24 11:22:44 +0300

    Write a function to remove several items from an array

--- javascript-basics/loops-and-arrays/08_removeFromArray/removeFromArray.js
+++ javascript-basics/loops-and-arrays/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray, ...itemsToRemove) {
+  let filteredArray = inputArray.filter((element) => {
+    return !itemsToRemove.includes(element);
+  });
+  return filteredArray;
 };
 
+console.log(removeFromArray([1, 2, 2, 3, 3, 4, 5, 6], 3, 2, 7, "tacos"));
 // Do not edit below this line
 module.exports = removeFromArray;
