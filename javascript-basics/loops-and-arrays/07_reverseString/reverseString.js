cfc2e1d92deb185c5705e953d411600b8f0b0dda
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2025-11-24 05:53:45 +0300

    Write a function to reverse a string
    Wrote a function reverseString to reverse a string
    to pass all the required tests.

--- javascript-basics/loops-and-arrays/07_reverseString/reverseString.js
+++ javascript-basics/loops-and-arrays/07_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
+const reverseString = function (inputString) {
+  let stringToArray = inputString.split(" ");
+  let reversedArray = stringToArray.map((element) => {
+    let innerElementToArray = element.split("");
+    let reversedInnerArray = innerElementToArray.reverse();
+    let reversedInnerString = reversedInnerArray.join("");
+    return reversedInnerString;
+  });
 
+  // Extract reversed elements from the last index
+  let reversedInputArray = [];
+  for (let start = reversedArray.length - 1; start >= 0; start--) {
+    reversedInputArray.push(reversedArray[start]);
+  }
+
+  let reversedInput = reversedInputArray.join(" ");
+  return reversedInput;
 };
 
+console.log(reverseString("hey baby boy"));
 // Do not edit below this line
 module.exports = reverseString;
