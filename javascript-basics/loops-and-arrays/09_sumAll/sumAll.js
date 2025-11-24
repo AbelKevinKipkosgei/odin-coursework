e88a14408f4b21d4c3af6411fc7f3b3e2d76e4ee
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2025-11-24 13:27:44 +0300

    Write a function to find sum of all numbers in a range
    Wrote a function to find the sum of all numbers in
    a range including the start and end numbers in the
    range.

--- javascript-basics/loops-and-arrays/09_sumAll/sumAll.js
+++ javascript-basics/loops-and-arrays/09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let sum = 0;
+  if (
+    [start, end].some(
+      (n) => typeof n !== "number" || !Number.isInteger(n) || n < 0
+    )
+  ) {
+    return `ERROR`;
+  }
 
+  if (start > end) {
+    for (let i = start; start >= end; start--) {
+      sum += start;
+    }
+    return sum;
+  } else if (start < end) {
+    for (let x = start; start <= end; start++) {
+      sum += start;
+    }
+    return sum;
+  }
 };
+
+console.log(sumAll(1, 4000));
 
 // Do not edit below this line
 module.exports = sumAll;
