98d4d6059f3c0bafbcc8e75b97b5a374823d7190
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2025-11-24 11:35:47 +0300

    Add a condition to check repeat times
    Added an if condition to check whether the number
    of times to repeat a string is less than zero to
    generate an ERROR message if that's the case.

--- javascript-basics/loops-and-arrays/06_repeatString/repeatString.js
+++ javascript-basics/loops-and-arrays/06_repeatString/repeatString.js
@@ -1,12 +1,15 @@
 const repeatString = function (stringToRepeat, numberOfTimesToRepeat) {
   let repeatedString = "";
+  if (numberOfTimesToRepeat < 0) {
+    return `ERROR`;
+  }
   for (let counter = 0; counter < numberOfTimesToRepeat; counter++) {
     repeatedString += stringToRepeat;
   }
   return repeatedString;
 };
 
-console.log(repeatString("hey", 3));
+console.log(repeatString("hey", -1));
 
 // Do not edit below this line
 module.exports = repeatString;
