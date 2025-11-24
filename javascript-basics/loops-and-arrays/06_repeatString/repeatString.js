fba0bdb2f00609161d1f49eece48a41e9a443e6f
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2025-11-24 04:03:28 +0300

    Write a function to repeat a string using loops

--- javascript-basics/loops-and-arrays/06_repeatString/repeatString.js
+++ javascript-basics/loops-and-arrays/06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (stringToRepeat, numberOfTimesToRepeat) {
+  let repeatedString = "";
+  for (let counter = 0; counter < numberOfTimesToRepeat; counter++) {
+    repeatedString += stringToRepeat;
+  }
+  return repeatedString;
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
