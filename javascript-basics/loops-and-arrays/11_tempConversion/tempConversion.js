0e91922aca082d5c1ba87b1a275f0b70ebd55a78
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2025-11-25 03:12:15 +0300

    Write functions to convert temperature units

--- javascript-basics/loops-and-arrays/11_tempConversion/tempConversion.js
+++ javascript-basics/loops-and-arrays/11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempFahrenheit) {
+  let tempInCelsius = (tempFahrenheit - 32) * (5 / 9);
+  degreesCelsius = Number(tempInCelsius.toFixed(1));
+  return degreesCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempCelsius) {
+  const tempInFahrenheit = Number((tempCelsius * (9 / 5) + 32).toFixed(1));
+  return tempInFahrenheit;
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(42));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
