adc2cb35d048d6b85c99db6b606967f4336dd971
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2026-03-12 13:15:24 +0300

    Add JavaScript code to implement DOM manipulation
    Add JavaScript code to implement DOM manipulation
    using Modifier keys such as: Ctrl, Shift, and Alt.

---
+++ javascript-basics/dom-manipulation-and-events/src/modifier-keys.js
@@ -0,0 +1,14 @@
+const btnKeys = document.querySelector("#btnKeys");
+
+btnKeys.addEventListener("click", (e) => {
+  let keys = [];
+
+  if (e.shiftKey) keys.push("shift");
+  if (e.ctrlKey) keys.push("ctrl");
+  if (e.altKey) keys.push("alt");
+  if (e.metaKey) keys.push("meta");
+
+  let msg = document.querySelector("#messageKeys");
+
+  msg.textContent = `Keys: ${keys.join("+")}`;
+});
